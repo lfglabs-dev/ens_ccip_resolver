fdb6f2ade3ba6f094e4faeec90884e745a882046
Thomas Marchand <thomas.marchand@tuta.io>
2024-11-16 20:52:39 +0700

    fix: deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
-import { vars, HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const PRIVATE_KEY = vars.get("PRIVATE_KEY");
-const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+dotenv.config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
